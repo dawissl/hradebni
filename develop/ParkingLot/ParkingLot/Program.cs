d6691ef26d4b29cdc48c264b74cb8446816bfe6a
Sládeček David <sladecek@hradebni.cz>
2021-06-15 08:51:04 +0200

    ParkingLot continue

--- develop/ParkingLot/ParkingLot/Program.cs
+++ develop/ParkingLot/ParkingLot/Program.cs
@@ -10,13 +10,120 @@ namespace ParkingLot
     {
         static void Main(string[] args)
         {
+
         }
     }
 
     class Parkoviste
     {
+        private Vozidlo[] parkovaciMista;
+        private int kapacita;
 
+        public Parkoviste()
+        {
+            parkovaciMista = new Vozidlo[3];
+            kapacita = 3;
+        }
 
+        public Parkoviste(int k)
+        {
+            kapacita = k;
+            parkovaciMista = new Vozidlo[kapacita];
+        }
+
+        public int PrijemZParkovani()
+        {
+            int suma = 0;
+            foreach (Vozidlo v in parkovaciMista)
+            {
+                if (v != null)
+                {
+                    switch (v.CarType)
+                    {
+                        case TypVozidla.BASIC:
+                            suma += 80;
+                            break;
+                        case TypVozidla.ELECTRO:
+                            suma += 60;
+                            break;
+                        case TypVozidla.PICKUP:
+                            suma += 10;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return suma;
+        }
+
+        public void VypisVozidel()
+        {
+
+        }
+
+        public bool JeMistoObsazene(int position)
+        {
+            if (!PositionCheck(position))
+            {
+                Console.WriteLine("Neznámé číslo parkovacího místa");
+                return false;
+            }
+
+            if (parkovaciMista[position] != null)
+            {
+                Console.WriteLine("Místo {1} je obsazene {0}", parkovaciMista[position].CarType, position);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Místo {0} je volne", position);
+                return true;
+            }
+        }
+
+        public void ZablokujMisto(int position)
+        {
+            if (!PositionCheck(position))
+            {
+                Console.WriteLine("Neznámé číslo parkovacího místa");
+                return;
+            }
+
+            if (parkovaciMista[position] != null)
+            {
+                Console.WriteLine("Místo je již obsazené");
+            }
+            else
+            {
+                parkovaciMista[position] = new Vozidlo();
+            }
+        }
+
+        public void Obsazenost() {
+            double obsazeno = 0;
+            foreach(Vozidlo v in parkovaciMista)
+            {
+                if (v != null) obsazeno++;     
+            }
+            double procentualniObsazeni = (obsazeno / kapacita) * 100;
+            Console.WriteLine("Obsazenost parkoviště: {0} %", procentualniObsazeni);
+        }
+
+        public void ZobrazVolnaMista()
+        {
+            string volna = "";
+            for(int i = 0; i < kapacita; i++)
+            {
+                if (parkovaciMista[i] == null) volna += i + "\t";
+            }
+            Console.WriteLine("Místa k dispozici:\n{0}",volna);
+        }
+
+        private bool PositionCheck(int num)
+        {
+            return num >= 0 && num < kapacita;
+        }
     }
 
     class Vozidlo
@@ -25,12 +132,14 @@ namespace ParkingLot
         private string owner;
         private TypVozidla carType;
 
+        public TypVozidla CarType { get { return carType; } }
+
         // konstruktor pro blokovane vozidlo
         public Vozidlo()
         {
-            licensePlate = "0XXXXXX";
+            licensePlate = "-------";
             owner = "blokované místo";
-            carType = TypVozidla.BASIC;
+            carType = TypVozidla.BLOCKED;
         }
 
         public Vozidlo(string owner, string licensePlate)
@@ -44,19 +153,32 @@ namespace ParkingLot
         {
             bool letter = false;
             bool digit = false;
-         
-                if(input.Length != 7)
-                {
-                    Console.WriteLine("Neplatná SPZ, zadejte znovu:");
-                    return LicensePlateCheck(Console.ReadLine());
-                }
 
-                for(int i = 0; i < 7; i++)
-                {
-                    
-                }
-           
-            return "";
+            if (input.Length != 7)
+            {
+                Console.WriteLine("Neplatná SPZ, zadejte znovu:");
+                return LicensePlateCheck(Console.ReadLine());
+            }
+            char[] plate = input.ToCharArray();
+            for (int i = 0; i < 7; i++)
+            {
+                if (Char.IsDigit(plate[i])) digit = true;
+                if (Char.IsLetter(plate[i])) letter = true;
+
+            }
+
+            if (!letter || !digit)
+            {
+                Console.WriteLine("Neplatná SPZ, zadejte znovu:");
+                return LicensePlateCheck(Console.ReadLine());
+            }
+
+            return input;
+        }
+
+        public override string ToString()
+        {
+            return licensePlate + " [" + carType + "]";
         }
     }
 
@@ -70,5 +192,5 @@ namespace ParkingLot
 
     }
 
-    enum TypVozidla { BASIC, ELECTRO, PICKPU, BLOCKED }
+    enum TypVozidla { BASIC, ELECTRO, PICKUP, BLOCKED }
 }
