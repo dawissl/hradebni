521345f6920a83751fd1eaea20b991cb001d6844
David <dawis.sl@gmail.com>
2021-06-17 17:19:11 +0200

    beta version parking lot

--- develop/ParkingLot/ParkingLot/Program.cs
+++ develop/ParkingLot/ParkingLot/Program.cs
@@ -10,8 +10,101 @@ namespace ParkingLot
     {
         static void Main(string[] args)
         {
-            
+            bool work = true;
+            Parkoviste parkoviste = new Parkoviste();
+            Info();
+
+            while (work)
+            {
+                Console.Write("Načtěte instrukci (HELP pro přehled instrukcí):");
+                string opt = Console.ReadLine().ToUpper();
+
+                switch (opt)
+                {
+                    case "CREATE":
+                        parkoviste = NoveParkoviste();
+                        break;
+                    case "ADD":
+                        parkoviste.Zaparkuj();
+                        break;
+                    case "REMOVE":
+                        parkoviste.Vyparkuj();
+                        break;
+                    case "BLOCK":
+                        parkoviste.ZablokujMisto();
+                        break;
+                    case "INCOME":
+                        Income(parkoviste);
+                        break;
+                    case "STATUS":
+                        Status(parkoviste);
+                        break;
+                    case "HELP":
+                        Help();
+                        break;
+                    case "EXIT":
+                        work = false;
+                        break;
+                    default:
+                        Console.WriteLine("Neznámá instrukce");
+                        break;
+
+
+                }
+                Oddelovac();
+            }
+
+
         }
+
+        private static void Info()
+        {
+            Console.WriteLine("=== Aplikace parkoviště ===");
+            Console.WriteLine("Jednoduchá správa parkoviště o zvolené velikost s možností zjištění poplatků");
+            Console.WriteLine("\t\t\t\t\t\t\tCreated by: David Sládeček");
+            Oddelovac();
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("=== Nápověda k ovládání ===");
+            Console.WriteLine("CREATE: přepsání původního parkoviště nově vytvořeným");
+            Console.WriteLine("ADD: zaparkování vozidla");
+            Console.WriteLine("\tTYP vozidla: BASIC, ELEKTRO, PIKUP");
+            Console.WriteLine("REMOVE: vyparkování vozidla / odblokování místa");
+            Console.WriteLine("BLOCK: zablokování parkovacího místa");
+            Console.WriteLine("INCOME: zobrazení příjmů z parkování");
+            Console.WriteLine("STATUS: přehled parkoviště");
+            Console.WriteLine("HELP: zobrazení nápovědy");
+            Console.WriteLine("CANCEL: přerušení aktuální instrukce");
+            Console.WriteLine("EXIT: ukončení aplikace");
+        }
+
+        private static void Status(Parkoviste p)
+        {
+            Console.WriteLine("=== Přehled parkoviště ===");
+            p.Obsazenost();
+            p.VypisVozidel();
+        }
+
+        private static void Oddelovac()
+        {
+            Console.WriteLine("===================================================================================");
+
+        }
+
+        private static Parkoviste NoveParkoviste()
+        {
+            Console.Write("Kapacita nového parkoviště (staré bude nahrazeno):");
+            return new Parkoviste(int.Parse(Console.ReadLine()));
+        }
+
+        private static void Income(Parkoviste p)
+        {
+            Console.WriteLine("Přijem z parkování: {0} Kč", p.PrijemZParkovani());
+        }
+
+
     }
 
     class Parkoviste
@@ -61,12 +154,22 @@ namespace ParkingLot
         {
             string parkoviste = "";
 
-            foreach (Vozidlo v in parkovaciMista)
+            for (int i = 0; i < kapacita; i++)
             {
-                if (v != null || v.CarType != TypVozidla.BLOCKED)
+                parkoviste += "[" + i + "]:";
+                if (parkovaciMista[i] != null)
                 {
-                    parkoviste += v;
+                    if (parkovaciMista[i].CarType != TypVozidla.BLOCKED)
+                    {
+                        parkoviste += parkovaciMista[i];
+                    }
+                    else
+                    {
+                        parkoviste += TypVozidla.BLOCKED;
+                    }
+
                 }
+                parkoviste += "\n";
             }
 
             Console.WriteLine(parkoviste);
@@ -86,7 +189,7 @@ namespace ParkingLot
             if (position == -1) return;
 
             Vozidlo v = CreateCar();
-            parkovaciMista[position] = v;
+            if (v != null) parkovaciMista[position] = v;
 
         }
 
@@ -95,33 +198,40 @@ namespace ParkingLot
             Console.WriteLine("Vyváření vozidla");
             Console.WriteLine("MAJITEL SPZ TYP [DOBA NABIJENI/NOSNOST]");
             string line = Console.ReadLine();
+            if (line.ToUpper() == "CANCEL") return null;
             string[] input = line.Split(' ');
-            if (input.Length > 4)
+            if (input.Length > 4 || input.Length < 3)
             {
                 Console.WriteLine("Neplatný formát vstupu");
                 CreateCar();
             }
 
-            try {
-               
+            try
+            {
+
                 switch (input[2])
                 {
                     case "BASIC":
-                        return new Vozidlo(input[0],input[1]);
-                    case "ELEKTRO":
-                        return new Elektromobil(input[0], input[1], input[3]);  
+                        return new Vozidlo(input[0], input[1]);
+                    case "ELEKTRO":     
+                        return new Elektromobil(input[0], input[1], input[3]);
                     case "PICKUP":
                         return new Dodavka(input[0], input[1], double.Parse(input[3]));
                     default:
-                        Console.Write("Neznamý typ vozidla");
+                        Console.WriteLine("Neznamý typ vozidla");
                         return CreateCar();
-                  
+
                 }
             }
             catch (FormatException e)
-            { 
+            {
                 Console.Write(e.Message);
-               return CreateCar(); 
+                return CreateCar();
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Nesprávný počet parametrů");
+                    return CreateCar();
             }
 
         }
@@ -141,9 +251,9 @@ namespace ParkingLot
 
         public void Vyparkuj(int pozice)
         {
-          
-                parkovaciMista[pozice] = null;
-          
+
+            parkovaciMista[pozice] = null;
+
         }
 
         public void Vyparkuj()
@@ -151,10 +261,16 @@ namespace ParkingLot
             Console.Write("Vyparkovat misto: ");
             try
             {
-                int position = int.Parse(Console.ReadLine());
+                string input = Console.ReadLine();
+                int position;
+                if (input.ToUpper() == "CANCEL") 
+                    return;
+                else
+                    position = int.Parse(input);
+
                 if (!PositionCheck(position))
                 {
-                   Vyparkuj();
+                    Vyparkuj();
                 }
                 else
                 {
@@ -206,6 +322,27 @@ namespace ParkingLot
             }
         }
 
+        public void ZablokujMisto()
+        {
+            Console.Write("Blokování místa č.: ");
+            try
+            {
+                string input = Console.ReadLine();
+                int position;
+                if (input.ToUpper() == "CANCEL")
+                    return;
+                else
+                    position = int.Parse(input);
+                ZablokujMisto(position);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Vložte číslo parkovacího místa!");
+                ZablokujMisto();
+            }
+
+        }
+
         public void Obsazenost()
         {
             double obsazeno = 0;
@@ -237,7 +374,7 @@ namespace ParkingLot
     {
         private string licensePlate;
         private string owner;
-        private TypVozidla carType;
+        protected TypVozidla carType;
 
         public TypVozidla CarType { get { return carType; } }
 
@@ -293,16 +430,20 @@ namespace ParkingLot
     {
         private string dobaNabijeni;
 
-        public Elektromobil(string owner, string licensePlate, string doba) : base(owner, licensePlate) {
+        public Elektromobil(string owner, string licensePlate, string doba) : base(owner, licensePlate)
+        {
             dobaNabijeni = doba;
-        } 
+            carType = TypVozidla.ELECTRO;
+        }
     }
 
     class Dodavka : Vozidlo
     {
         private double maxNosnost;
-        public Dodavka(string owner, string licensePlate, double zatizeni) : base(owner, licensePlate) {
+        public Dodavka(string owner, string licensePlate, double zatizeni) : base(owner, licensePlate)
+        {
             maxNosnost = zatizeni;
+            carType = TypVozidla.PICKUP;
         }
     }
 
