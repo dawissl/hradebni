c20814307233defb6df3e6de3742e2dacdde0b11
Sládeček David <sladecek@hradebni.cz>
2021-09-21 09:37:14 +0200

    number analyzer - 2A sk2

---
+++ 2021-2022/2.A_sk2/AnalyzaCisla/AnalyzaCisla/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AnalyzaCisla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtNumber = new System.Windows.Forms.TextBox();
+            this.BtnAnalyze = new System.Windows.Forms.Button();
+            this.LblAnalyze = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtNumber
+            // 
+            this.TxtNumber.Location = new System.Drawing.Point(130, 28);
+            this.TxtNumber.Name = "TxtNumber";
+            this.TxtNumber.Size = new System.Drawing.Size(188, 20);
+            this.TxtNumber.TabIndex = 0;
+            // 
+            // BtnAnalyze
+            // 
+            this.BtnAnalyze.Location = new System.Drawing.Point(130, 75);
+            this.BtnAnalyze.Name = "BtnAnalyze";
+            this.BtnAnalyze.Size = new System.Drawing.Size(196, 86);
+            this.BtnAnalyze.TabIndex = 1;
+            this.BtnAnalyze.Text = "Analyzuj číslo";
+            this.BtnAnalyze.UseVisualStyleBackColor = true;
+            this.BtnAnalyze.Click += new System.EventHandler(this.BtnAnalyze_Click);
+            // 
+            // LblAnalyze
+            // 
+            this.LblAnalyze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblAnalyze.Location = new System.Drawing.Point(29, 181);
+            this.LblAnalyze.Name = "LblAnalyze";
+            this.LblAnalyze.Size = new System.Drawing.Size(400, 129);
+            this.LblAnalyze.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 358);
+            this.Controls.Add(this.LblAnalyze);
+            this.Controls.Add(this.BtnAnalyze);
+            this.Controls.Add(this.TxtNumber);
+            this.Name = "Form1";
+            this.Text = "Analyzátor čísla";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtNumber;
+        private System.Windows.Forms.Button BtnAnalyze;
+        private System.Windows.Forms.Label LblAnalyze;
+    }
+}
+
---
+++ 2021-2022/2.A_sk2/AnalyzaCisla/AnalyzaCisla/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalyzaCisla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnAnalyze_Click(object sender, EventArgs e)
+        {
+            string result = "Pro číslo platí: ";
+            int number = int.Parse(TxtNumber.Text);
+            result += CheckEvenOdd(number);
+            result += CheckPrime(number);
+            result += CheckPositiveNegative(number);
+            result += CheckDivider(number);
+            result += CheckPerfect(number);
+            LblAnalyze.Text = result;
+        }
+
+        private string CheckPerfect(int number)
+        {
+            int suma = 1;
+            for (int i = 2; i < number; i++)
+            {
+                if((number % i) == 0)
+                {
+                    suma += i;
+                }
+            }
+            if(suma == number)
+            {
+                return "dokonalé číslo";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private string CheckDivider(int number)
+        {
+            return "dividers";
+        }
+
+        private string CheckPositiveNegative(int number)
+        {
+            if (number > 0)
+            {
+                return "kladné, ";
+            }
+            else
+            {
+                return "záporné, ";
+            }
+        }
+
+        private string CheckPrime(int number)
+        {
+            return "prime";
+        }
+
+        private string CheckEvenOdd(int number)
+        {
+            if ((number % 2) == 0)
+            {
+                return "sudé, ";
+            }
+            else
+            {
+                return "liché, ";
+            }
+        }
+    }
+}
