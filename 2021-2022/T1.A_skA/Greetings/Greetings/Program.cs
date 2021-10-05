fe97175549825555375c821d0eab5018b87127c7
David Sládeček <dawis.sl@gmail.com>
2021-10-05 07:55:57 +0200

    greetings app

---
+++ 2021-2022/T1.A_skA/Greetings/Greetings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno;
+            int vek;
+            Console.WriteLine("Napiš své jméno");
+            jmeno = Console.ReadLine();
+            Console.WriteLine("Napiš svůj věk");
+            vek = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ahoj {0}, Tobě je už {1} roků?", jmeno, vek);
+        }
+    }
+}
---
+++ 2021-2022/T1.A_skB/Greetings/Greetings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno;
+            int vek;
+            Console.WriteLine("Zadej své jméno");
+            jmeno = Console.ReadLine();
+            Console.WriteLine("Zadej svůj věk");
+            vek = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ahoj {0}, Tobě je už {1} roků?", jmeno, vek);
+        }
+    }
+}
