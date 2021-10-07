002b77a3151f427538fdbdc3a4ded2ee1286cd6d
David Sládeček <dawis.sl@gmail.com>
2021-10-07 12:27:07 +0200

    dummy calc

---
+++ 2021-2022/T1.A_skA/DummyCalc/DummyCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DummyCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarovná proměnných a,b,c
+            double a, b, c;
+            // deklarace pole se třemi prvky
+            double[] cisla = new double[3];
+
+            // načíst hodnoty od uživatele a vložit do a,b,c
+            Console.WriteLine("Zadejte tři čísla");
+            cisla[0] = double.Parse(Console.ReadLine());
+            cisla[1] = double.Parse(Console.ReadLine());
+            cisla[2] = double.Parse(Console.ReadLine());
+
+            // deklarace proměnných suma, součin, rozdíl
+            // vložení výpočtu do proměnných
+
+            double suma = cisla[0] + cisla[1] + cisla[2];
+            double rozdil = cisla[0] - cisla[1] - cisla[2];
+            double soucin = cisla[0] * cisla[1] * cisla[2];
+
+            Console.WriteLine("Suma je: {0}", suma );
+            Console.WriteLine("Rozdíl je: {0}", rozdil);
+            Console.WriteLine("Součin je: {0}", soucin);
+
+        }
+    }
+}
---
+++ 2021-2022/T1.A_skB/DummyCalc/DummyCalc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DummyCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarace proměnných a,b,c
+            double a, b, c;
+            // deklarace pole se třemi položkami
+            double[] cisla = new double[3];
+            Console.WriteLine("Zadejte tři celá čísla");
+            /* 
+             * načtení hodnot do proměnných a,b,c
+             * funkce Console.ReadLine(); nám vráti řetězec a musíme 
+             * hodnotu z řetězce vyparsovat pomocí funkce Parse
+            */
+            cisla[0] = double.Parse(Console.ReadLine());
+            cisla[1] = double.Parse(Console.ReadLine());
+            cisla[2] = double.Parse(Console.ReadLine());
+          
+
+            // deklarace proměnných suma, rozdíl, součin
+            double suma = cisla[0] + cisla[1] + cisla[2];
+            double rozdil = cisla[0] - cisla[1] - cisla[2];
+            double soucin = cisla[0] * cisla[1] * cisla[2];
+
+            // vypis výpočtů
+            Console.WriteLine("Suma čísel je: {0}", suma);
+            Console.WriteLine("Rozdíl čísel je: {0}", rozdil);
+            Console.WriteLine("Součin čísel je: {0}", soucin);
+
+
+
+        }
+    }
+}
