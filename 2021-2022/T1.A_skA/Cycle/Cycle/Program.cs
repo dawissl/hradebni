99394f8aee6bd93e7133b6535ad5bdc8ce6c2641
David Sládeček <dawis.sl@gmail.com>
2021-10-21 12:38:49 +0200

    21.10.21

---
+++ 2021-2022/T1.A_skA/Cycle/Cycle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Zadejte číslo:");
+            // double a = double.Parse(Console.ReadLine());
+
+            // while
+
+            /* 
+             * while (a >= 0)
+             {
+                 a = a - 3.14;
+                 Console.WriteLine(a);
+             }*/
+
+
+            // do-while
+            /*
+            do
+            {
+                a = a - 3.14;
+                Console.WriteLine(a);
+            } while (a >= 0);
+            */
+
+            int cislo;
+            do
+            {
+                Console.Write("Zadejte číslo: ");
+                cislo = int.Parse(Console.ReadLine());
+
+                if (cislo >= 0)
+                {
+                    Console.WriteLine("+");
+                }
+                else
+                {
+                    Console.WriteLine("-");
+
+                }
+            } while (cislo != 0);
+        }
+
+
+    }
+}
