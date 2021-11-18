015fbad4ab0f9b03b968a9af8fb7d549ffeaf5ac
David Sládeček <dawis.sl@gmail.com>
2021-11-18 12:37:02 +0100

    switch, basic interface, arguments of main function

---
+++ 2021-2022/T1.A_skA/PrekladacZnaku/PrekladacZnaku/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PrekladacZnaku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // použiti proměnné pro kontrolo, zda má aplikace běžet
+            bool work = true;
+            //dokud je proměnná pravdivá probíhá cyklus
+            while (work)
+            {
+                Console.Write("Vložte číslo:");
+                //string znak = Console.ReadLine();
+                int day = int.Parse(Console.ReadLine());
+
+                switch (day)
+                {
+                    // v případě, že uživatel zadá hodnotu -1
+                    // proměnná work je nastavena na false
+                    // podmínka cyklu nebude splněná a aplikace
+                    // se ukončí
+                    case -1:
+                        Console.WriteLine("ukončování aplikace");
+                        work = false;
+                        break;
+                        //zadáním instrukce se volá funkce, kterou máme
+                        //implementovanou - zobrazení nápovědy
+                    case 1:
+                        ZobrazNapovedu();
+                        break;
+                    case 2:
+                        Console.WriteLine("úterý");
+                        break;
+                    case 3:
+                        Console.WriteLine("středa");
+                        break;
+                    case 4:
+                        Console.WriteLine("čtvrtek");
+                        break;
+                    case 5:
+                        Console.WriteLine("pátek");
+                        break;
+                    case 6:
+                        Console.WriteLine("sobota");
+                        break;
+                    case 7:
+                        Console.WriteLine("neděle");
+                        break;
+                    default:
+                        Console.WriteLine("neznámý den");
+                        break;
+
+                }
+            }
+
+        }
+
+        //pomocná funkce pro zobrazení nápovědy
+        private static void ZobrazNapovedu()
+        {
+            Console.WriteLine("nápověda aplikace");
+        }
+    }
+}
---
+++ 2021-2022/T1.A_skB/PrekladacZnaku/PrekladacZnaku/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PrekladacZnaku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // vuziti pomocné proměnné identifikující zda má aplikace stále pokračovat
+            bool work = true;
+            // dokud je proměnná true, aplikace bude stále běžet
+            while (work)
+            {
+                //načtení instrukce od uživatele
+                Console.WriteLine("Vložte číslo");
+                int day = int.Parse(Console.ReadLine());
+
+                //využití konstrukce switch-case pro rozhodnutí, co se má stát
+                //dle vstupní hodnoty
+                switch (day)
+                {
+                    case 1:
+                        //volání funkce pro zobrazení nápovědy
+                        ShowHelp();
+                        break;
+                    case 2:
+                        Console.WriteLine("instrukce 2");
+                        break;
+                        //v případě zadání 666 je proměnná nastavena na false
+                        //v další kontrole podmínky while cyklu nebude
+                        //podmínka splněna a aplikace se ukončí
+                    case 666:
+                        Console.WriteLine("ukončení aplikace...");
+                        work = false;
+                        break;
+                    default:
+                        Console.WriteLine("Neznámá instrukce");
+                        break;
+
+                }
+            }
+        }
+
+        //pomocná funkce, která nám zobrazí nápovědo
+        private static void ShowHelp()
+        {
+            Console.WriteLine("nápověda pro praci s aplikaci");
+        }
+    }
+}
