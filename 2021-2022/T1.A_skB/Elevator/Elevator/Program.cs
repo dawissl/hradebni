1882e69ad1b809adb9e1bb94f32f64f4942755cd
David Sládeček <dawis.sl@gmail.com>
2022-02-01 14:16:41 +0100

    elevator

---
+++ 2021-2022/T1.A_skB/Elevator/Elevator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikace výtah");
+            int vaha = 0;
+            int pocetLidi = 0;
+
+            while (vaha < 800 && pocetLidi < 8)
+            {
+                Console.Write("Vložte váhu dalšího člověka: ");
+                int novaVaha = int.Parse(Console.ReadLine());
+                if((vaha + novaVaha) > 800)
+                {
+                    Console.WriteLine("Nelze nastoupit, překročen váhový limit!");
+                   continue;
+                }
+                vaha += novaVaha;
+                pocetLidi++;
+            }
+
+            Console.WriteLine($"Do vytahu nastoupilo {pocetLidi} lidi a jejich váha je {vaha}kg");
+
+        }
+    }
+}
