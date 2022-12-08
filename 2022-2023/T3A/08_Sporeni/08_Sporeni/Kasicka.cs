2c72ea18cb31d715b0020bfd533db67bb9a93d23
David Sládeček <dawis.sl@gmail.com>
2022-12-08 13:49:01 +0100

    sporeni

--- 2022-2023/T3A/08_Sporeni/08_Sporeni/Kasicka.cs
+++ 2022-2023/T3A/08_Sporeni/08_Sporeni/Kasicka.cs
@@ -17,7 +17,54 @@ namespace _08_Sporeni
         public Kasicka()
         {
             obalkaList = new List<Obalka>();
+            obalkaList.Add(new Obalka(100, "Výchozí obálka"));
             money = 0;
+        }
+
+        internal void VlozitObalku(Obalka ob)
+        {
+            int procentaNavic = 0;
+            int odectenaProcenta = ob.Percent / obalkaList.Count;
+            foreach (Obalka o in obalkaList)
+            {
+                if (o.Percent - odectenaProcenta <= 0)
+                {
+                    procentaNavic += (odectenaProcenta - 1);
+                    o.Percent = 1;
+
+                }
+
+                else
+                    o.Percent -= odectenaProcenta;
+            }
+
+            while (procentaNavic > 0)
+            {
+                odectenaProcenta = procentaNavic / obalkaList.Count;
+                foreach (Obalka o in obalkaList)
+                {
+                    if (o.Percent - odectenaProcenta > 1)
+                    {
+                        o.Percent -= odectenaProcenta;
+                        procentaNavic -= odectenaProcenta;
+                    }
+
+                }
+
+            }
+            obalkaList.Add(ob);
+
+        }
+
+        internal void VlozitPenize(int m)
+        {
+            Money = m;
+            double onePercente = m / 100;
+
+            foreach (Obalka o in obalkaList)
+            {
+                o.Money = onePercente * o.Percent;
+            }
         }
     }
 }
