7f0915e23007ecad3114e63f5d146acb6107a774
David Sládeček <sladecek@hradebni.cz>
2024-06-20 11:36:19 +0200

    prep for new year

---
+++ develop/2024-25/Demo_plneni_vodou/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Objem nádoby:");
+            double size = double.Parse(Console.ReadLine());
+            Console.WriteLine("Do nádoby o objemu 100 l naleji 4x vodu pomocí zvolené nádoby");
+            double value = 0;
+            Console.WriteLine($"Naplněnost nádoby {value}/100 l");
+            Console.WriteLine("První plnění");
+            value += size;
+            if (value > 100)
+            {
+                Console.WriteLine("Nádoba je plná");
+            }
+            else
+            {
+                Console.WriteLine($"Naplněnost nádoby {value}/100 l");
+               
+            }
+            Console.WriteLine("Druhé plnění");
+            value += size;
+            if (value > 100)
+            {
+                Console.WriteLine("Nádoba je plná");
+            }
+            else
+            {
+                Console.WriteLine($"Naplněnost nádoby {value}/100 l");
+
+            }
+            Console.WriteLine("Třetí plnění");
+            value += size;
+            if (value > 100)
+            {
+                Console.WriteLine("Nádoba je plná");
+            }
+            else
+            {
+                Console.WriteLine($"Naplněnost nádoby {value}/100 l");
+
+            }
+            Console.WriteLine("Čtvrté plnění");
+            value += size;
+            if (value > 100)
+            {
+                Console.WriteLine("Nádoba je plná");
+            }
+            else
+            {
+                Console.WriteLine($"Naplněnost nádoby {value}/100 l");
+
+            }
+        }
+    }
+}
