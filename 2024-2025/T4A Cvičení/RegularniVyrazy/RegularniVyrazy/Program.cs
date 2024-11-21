796ad86561e2184e2a4089c2d708c184e297eec2
Sládeček David <sladecek@hradebni.cz>
2024-11-21 09:24:13 +0100

    RegEx console

---
+++ 2024-2025/T4A Cvičení/RegularniVyrazy/RegularniVyrazy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularniVyrazy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte vstupní řetězec pro kontrolu:");
+            string vstup = Console.ReadLine();
+            //string vzor = @"\b[a-z]+@[a-z]+\.(cz|com|org)\b";
+            //string vzor = @"\b[^5-9]+\b";
+           // string vzor = @"\bx[a-z]?\b";
+           string vzor = @"\b(ab)+(cd)+\b";
+            MatchCollection shody = Regex.Matches(vstup, vzor);
+            foreach (Match match in shody) {
+                Console.WriteLine($"Shoda {match.Value} na pozici {match.Index}");
+            }
+        }
+    }
+}
