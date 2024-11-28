8e3082417a17d5ab8a4d975954846f6f276679e5
David Sládeček <sladecek@hradebni.cz>
2024-11-28 12:52:30 +0100

    08_dokonaleCislo

---
+++ 2024-2025/S1T/08_DokonaleCislo/08_DokonaleCislo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08_DokonaleCislo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- 08_DokonaleCislo ----");
+            Console.WriteLine("Zadejte číslo, zda je dokonalé");
+            int cislo = int.Parse(Console.ReadLine());
+            int suma = 1; // proměnná suma, kam budeme přičítat dělitele
+            // využíváme toho, že každé číslo je dělitelné číslem 1
+            for(int i=2; i<cislo; i++)
+            {
+                if(cislo % i == 0) // pokud je číslo dělitelem, přičteme jej
+                {
+                    suma += i;
+                }
+            }
+            if (suma == cislo) // pokud je suma vstupní číslo, je číslo dokonalé
+            {
+                Console.WriteLine("Číslo je dokonalé");
+            }
+            else
+            {
+                Console.WriteLine("Číslo není dokonalé");
+            }
+        }
+    }
+}
