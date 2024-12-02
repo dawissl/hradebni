6de1f66daf913c0b2aa8dc5b6b072c91ec725e6a
David Sládeček <sladecek@hradebni.cz>
2024-12-02 13:26:15 +0100

    20241202 lectures

---
+++ 2024-2025/T1Aa/09_Tyden/09_Tyden/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _09_Tyden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string instrukce;
+            // | = alt + 124
+            // [ = altGr + F
+            // ] = altGr + G
+            do
+            {
+                Console.Write("Zvol instrukci [EN|CS|KONEC]: ");
+                instrukce = Console.ReadLine().ToUpper();
+                int den = 0;
+                if (instrukce == "EN" || instrukce == "CS") // den si vyžádáme pouze pokud byl zvolen jazyk
+                {
+                    Console.Write("Zadej číslo dne v týdnu: ");
+                    den = int.Parse(Console.ReadLine());
+                }
+                if (instrukce == "EN")
+                {
+                    // switch s anglickym tydnem
+                    switch (den)
+                    {
+                        case 1:
+                            Console.WriteLine("It is a Sunday");
+                            break;
+                        case 2:
+                            Console.WriteLine("It is a Monday");
+                            break;
+                        case 3:
+                            Console.WriteLine("It is a Tuesday");
+                            break;
+                        case 4:
+                            Console.WriteLine("It is a Wednesday");
+                            break;
+                        case 5:
+                            Console.WriteLine("It is a Thursday");
+                            break;
+                        case 6:
+                            Console.WriteLine("It is a Friday");
+                            break;
+                        case 7:
+                            Console.WriteLine("It is a Saturday");
+                            break;
+                        default:
+                            Console.WriteLine("It is not a day");
+                            break;
+                    }
+                }
+                else if (instrukce == "CS")
+                {
+                    switch (den)
+                    {
+                        case 1:
+                            Console.WriteLine("Pondělí");
+                            break;
+                        case 2:
+                            Console.WriteLine("Úterý");
+                            break;
+                        case 3:
+                            Console.WriteLine("Středa");
+                            break;
+                        case 4:
+                            Console.WriteLine("Čtvrtek");
+                            break;
+                        case 5:
+                            Console.WriteLine("Pátek");
+                            break;
+                        case 6:
+                            Console.WriteLine("Sobota");
+                            break;
+                        case 7:
+                            Console.WriteLine("Neděle");
+                            break;
+                        default:
+                            Console.WriteLine("Neexistující den");
+                            break;
+                    }
+                }
+                else if (instrukce == "KONEC")
+                {
+                    // v případě, že přijde instrukce konec, přeskakujeme zbytek kódu
+                    // a kontrolujeme podmínku cyklu
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Neznámá operace");
+                }
+            } while (instrukce != "KONEC");
+
+        }
+    }
+}
---
+++ 2024-2025/T1Ab/09_Tyden/09_Tyden/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _09_Tyden
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // proměnná instrukce rozhodujici o pokračování nebo ukončení aplikace / do-while cyklu
+
+            string instrukce; 
+            do
+            {
+                // | = alt + 124
+                // [ = altGr + F
+                // ] = altGr + G
+                Console.Write("Zvol instrukci [EN|CS|EXIT]: ");
+                instrukce = Console.ReadLine().ToUpper();
+                if (instrukce == "EXIT") continue;
+                Console.Write("Zadej číslo dne v týdnu: ");
+                int den = int.Parse(Console.ReadLine());
+                if (instrukce == "EN")
+                {
+                    // switch s nedělí jako první den
+                    switch (den)
+                    {
+                        case 1:
+                            Console.WriteLine("It is a Sunday");
+                            break;
+                        case 2:
+                            Console.WriteLine("It is a Monday");
+                            break;
+                        case 3:
+                            Console.WriteLine("It is a Tuesday");
+                            break;
+                        case 4:
+                            Console.WriteLine("It is a Wednesday");
+                            break;
+                        case 5:
+                            Console.WriteLine("It is a Thursday");
+                            break;
+                        case 6:
+                            Console.WriteLine("It is a Friday");
+                            break;
+                        case 7:
+                            Console.WriteLine("It is a Saturday");
+                            break;
+                        default:
+                            Console.WriteLine("It is not a day");
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (den)
+                    {
+                        case 1:
+                            Console.WriteLine("Pondělí");
+                            break;
+                        case 2:
+                            Console.WriteLine("Úterý");
+                            break;
+                        case 3:
+                            Console.WriteLine("Středa");
+                            break;
+                        case 4:
+                            Console.WriteLine("Čtvrtek");
+                            break;
+                        case 5:
+                            Console.WriteLine("Pátek");
+                            break;
+                        case 6:
+                            Console.WriteLine("Sobota");
+                            break;
+                        case 7:
+                            Console.WriteLine("Neděle");
+                            break;
+                        default:
+                            Console.WriteLine("Neexistující den");
+                            break;
+
+                    }
+
+                }
+            } while (instrukce != "EXIT");
+        }
+    }
+}
