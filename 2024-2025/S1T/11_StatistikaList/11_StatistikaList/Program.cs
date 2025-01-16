f7c8d36579451b0ca71428dc8cd3134995cd048b
David Sládeček <sladecek@hradebni.cz>
2025-01-16 13:29:21 +0100

    list, functions

---
+++ 2024-2025/S1T/11_StatistikaList/11_StatistikaList/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _11_StatistikaList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* double[] pole = new double[10];
+             List<string> osoby = new List<string>();
+
+             // přídání položek do kolekce list
+             osoby.Add("Kamil");
+             osoby.Add("Eliška");
+             osoby.Add("Jan");
+             osoby.Add("Lucie");
+
+             // výpis položek
+             foreach (string p in osoby)
+             {
+                 Console.WriteLine(p);
+             }
+
+             // odstranění položek
+             osoby.Remove("Kamil");
+             osoby.RemoveAt(2);
+             Console.WriteLine("-----------------");
+             foreach (string p in osoby)
+             {
+                 Console.WriteLine(p);
+             }
+             // přístup k položkám v kolekci
+             Console.WriteLine("-----------------");
+             for (int i = 0; i < osoby.Count; i++)
+             {
+                 osoby[i] += $" {20 + i}";
+                 Console.WriteLine(osoby[i]);
+             }
+             // využítí předdefinovaných funkcí
+             if (osoby.Contains("Kamil"))
+                 Console.WriteLine("Kamil je přítomen");
+             else
+                 Console.WriteLine("Kamil není přítomen");
+
+             if (osoby.Contains("Eliška 20"))
+                 Console.WriteLine("Eliška je přítomna");
+             else
+                 Console.WriteLine("Eliška není přítomna");
+             */
+            Console.WriteLine("11_StatistikaList");
+            Console.WriteLine("Vkládej číselné hodnoty," +
+                " ukončení zadávání nastane při vložení čísla 0");
+            List<double> cisla = new List<double>();
+            double num = double.Parse(Console.ReadLine());
+            while(num != 0)
+            {
+                cisla.Add(num);
+                num = double.Parse(Console.ReadLine());
+            }
+            if (cisla.Count > 0)
+                Console.WriteLine($"Max: {cisla.Max()}, Min: {cisla.Min()}, Průměr: {cisla.Average()}, Součet: {cisla.Sum()}");
+            else
+                Console.WriteLine("Kolekce je prázdná");
+        }
+    }
+}
---
+++ 2024-2025/S1T/12_Funkce/12_Funkce/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _12_Funkce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrvniFunkce());
+            int cislo1 = 10;
+            int cislo2 = 30;
+            int soucet = SoucetCisel(cislo1, cislo2, 50);
+            Console.WriteLine(soucet);
+            Console.WriteLine(LzeSestrojitTrojuhelnik(6, 3, 7));
+        }
+
+        private static bool LzeSestrojitTrojuhelnik(int v1, int v2, int v3)
+        {
+            return v1 + v2 > v3 && v1 + v3 > v2 && v3 + v2 > v1;
+        }
+
+        public static string PrvniFunkce()
+        {
+            return "Ahoj";
+        }
+
+        public static int SoucetCisel(int a, int b, int c)
+        {
+            // a = cislo1, b = cislo2, c = 50
+            int suma = a + b + c;
+            return suma;
+        }
+    }
+}
